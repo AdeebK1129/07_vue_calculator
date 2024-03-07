c919811ebdd0c424bb0e744a6f250a4022f3935e
Adeeb <adeeb19909@gmail.com>
2024-03-07 09:44:04 -0500

    finalizing all main functionalities of the project

--- app.js
+++ app.js
@@ -4,24 +4,65 @@ createApp({
   data() {
     return {
       inputField: '',
+      undoStack: [],
+      redoStack: [],
     };
   },
   computed: {
-
+    convertExpression() {
+      return this.inputField.replace(/π/g, 'pi').replace(/√/g, 'sqrt');
+    }
   },
   watch: {
-
+    inputField(newVal, oldVal) {
+      const validInputPattern = /^[0-9π().+\-/*^%pq√]*$/;
+      if (!validInputPattern.test(newVal)) {
+        alert('Invalid input. Only numbers, parentheses, and basic operators are accepted.');
+        this.inputField = oldVal; 
+      } else {
+        if (newVal.includes('p')) {
+          this.inputField = newVal.replace(/p/g, 'π');
+        }
+        if (newVal.includes('q')) {
+          this.inputField = newVal.replace(/q/g, '√(');
+        }
+      }
+    }
   },
   methods: {
     backspace(){
       this.inputField = this.inputField.slice(0, -1);
     },
-    calculateExpression(){
-      this.inputField = math.evaluate(this.inputField);
+    calculateExpression() {
+      this.undoStack.push(this.inputField); 
+      try {
+        this.inputField = math.evaluate(this.convertExpression).toString();
+        this.redoStack = []; 
+      } catch (error) {
+        console.error('Invalid expression:', error);
+        alert('Invalid expression');
+      }
     },
     input(digit){
       this.inputField += digit;
     },
-
+    clear(){
+      this.inputField = '';
+    },
+    undo() {
+      if (this.undoStack.length > 0) {
+        const lastExpression = this.undoStack.pop();
+        this.redoStack.push(this.inputField); 
+        this.inputField = lastExpression; 
+      }
+    },
+    
+    redo() {
+      if (this.redoStack.length > 0) {
+        const lastUndoneExpression = this.redoStack.pop();
+        this.undoStack.push(this.inputField); 
+        this.inputField = lastUndoneExpression; 
+      }
+    },
   },
 }).mount('#app') 
